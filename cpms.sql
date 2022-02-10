ea27e4f4924df216673da8abd3855b4311fc23d9
abhinandanacharya <acharyaabhinandan41@gmail.com>
2022-02-10 21:40:48 +0530

    first commit

---
+++ cpms.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 10, 2022 at 04:59 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cpms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 1234567890, 'adminuser@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2020-04-14 06:44:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `ID` int(10) NOT NULL,
+  `CategoryName` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`ID`, `CategoryName`, `CreationDate`) VALUES
+(1, 'Logistic Deliverie', '2022-02-06 07:27:00'),
+(2, 'Cleaning', '2022-02-06 07:49:09'),
+(3, 'Essential Services', '2022-02-06 07:49:22'),
+(4, 'eccomerce delivery boys', '2022-02-06 07:49:47'),
+(5, 'Medical Supply', '2022-02-06 07:50:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpass`
+--
+
+CREATE TABLE `tblpass` (
+  `ID` int(10) NOT NULL,
+  `PassNumber` varchar(200) DEFAULT NULL,
+  `FullName` varchar(200) DEFAULT NULL,
+  `ContactNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `IdentityType` varchar(200) DEFAULT NULL,
+  `IdentityCardno` varchar(200) DEFAULT NULL,
+  `Category` varchar(100) DEFAULT NULL,
+  `FromDate` varchar(200) DEFAULT NULL,
+  `ToDate` varchar(200) DEFAULT NULL,
+  `PasscreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblpass`
+--
+
+INSERT INTO `tblpass` (`ID`, `PassNumber`, `FullName`, `ContactNumber`, `Email`, `IdentityType`, `IdentityCardno`, `Category`, `FromDate`, `ToDate`, `PasscreationDate`) VALUES
+(1, '286529906', 'Ram', 4654464646, 'ram@gmail.com', 'Adhar Card', 'AD-122346', 'Cleaning', '2022-02-06', '2022-02-14', '2020-04-14 07:47:03'),
+(2, '915773340', 'Shyam', 9879878978, 'shyam@gmail.com', 'Any Other Govt Issued Doc', 'KTI-896567', 'Essential Services', '2022-02-06', '2022-02-16', '2022-02-06 07:50:15'),
+(3, '884595667', 'Mohan', 1234567890, 'mohan@Gmail.com', 'Voter Card', '5235252', 'Essential Services', '2022-02-06', '2022-02-17', '2020-04-16 08:38:27'),
+(4, '869446071', 'Xyz', 123789456, 'xyz@gmail.com', 'PAN Card', '656565656', 'Medical Supply', '2022-02-06', '2022-02-10', '2022-02-06 08:59:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpass`
+--
+ALTER TABLE `tblpass`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblpass`
+--
+ALTER TABLE `tblpass`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
